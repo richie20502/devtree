ab22cc2e7f3c049cf2a784de24f213501f9b41cd
richie20502 <richie.lugo.recillas.1990@gmail.com>
2025-09-10 23:37:58 -0600

    campo handle

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,4 +1,5 @@
 import type {Request, Response} from 'express'
+import slug from 'slug'
 import User from "../models/User";
 import { hashPassword } from '../utils/auth';
 
@@ -6,11 +7,20 @@ export const createAccount =  async (request: Request, response: Response) => {
     const { email, password } = request.body;
     const userExist = await User.findOne({email});
     if(userExist){
-        const error = new Error('El Usuario ya esta registrado !!!');
+        const error = new Error('El usuario con ese mail ya esta registrado !!!');
         return response.status(409).json({error: error.message});
     }
+    const handle = slug(request.body.handle,'');
+    const handleExist = await User.findOne({handle});
+    if(handleExist){
+        const error = new Error('Nombre de usuario no disponible !!!');
+        return response.status(409).json({error: error.message});
+    }
+
     const user = new User(request.body);
     user.password = await hashPassword(password);
+    user.handle = handle;
+    console.log(slug(handle));
     await user.save();
     response.status(201).json({ msg: "Registro Creado Correctamente" });
     }
