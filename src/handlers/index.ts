2c1071cfe3d5e23e87a1ef9536d40c35f5df353d
richie20502 <richie.lugo.recillas.1990@gmail.com>
2025-09-10 22:17:59 -0600

    hash de passwords

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,15 +1,16 @@
 import type {Request, Response} from 'express'
 import User from "../models/User";
+import { hashPassword } from '../utils/auth';
 
 export const createAccount =  async (request: Request, response: Response) => {
-    const { email } = request.body;
+    const { email, password } = request.body;
     const userExist = await User.findOne({email});
     if(userExist){
         const error = new Error('El Usuario ya esta registrado !!!');
         return response.status(409).json({error: error.message});
     }
-
     const user = new User(request.body);
+    user.password = await hashPassword(password);
     await user.save();
     response.status(201).json({ msg: "Registro Creado Correctamente" });
     }
