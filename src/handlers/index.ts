c8387e0607c0b1ea04a16e4395d674e495d8c301
richie20502 <richie.lugo.recillas.1990@gmail.com>
2025-09-10 21:58:48 -0600

    evita usuarios duplicados

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,7 +2,14 @@ import type {Request, Response} from 'express'
 import User from "../models/User";
 
 export const createAccount =  async (request: Request, response: Response) => {
+    const { email } = request.body;
+    const userExist = await User.findOne({email});
+    if(userExist){
+        const error = new Error('El Usuario ya esta registrado !!!');
+        return response.status(409).json({error: error.message});
+    }
+
     const user = new User(request.body);
     await user.save();
-    response.json({ msg: "Registro Creado Correctamente" });
+    response.status(201).json({ msg: "Registro Creado Correctamente" });
     }
