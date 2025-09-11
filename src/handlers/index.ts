1dfa6a1789714614756095706eac04852225ce1a
richie20502 <richie.lugo.recillas.1990@gmail.com>
2025-09-10 21:41:45 -0600

    se agrego types  a routing

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,6 +1,7 @@
+import type {Request, Response} from 'express'
 import User from "../models/User";
 
-export const createAccount =  async (request, response) => {
+export const createAccount =  async (request: Request, response: Response) => {
     const user = new User(request.body);
     await user.save();
     response.json({ msg: "Registro Creado Correctamente" });
