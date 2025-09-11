1f60740fee0c341c16995b63cfb450350cc259fa
richie20502 <richie.lugo.recillas.1990@gmail.com>
2025-09-10 20:59:47 -0600

    se genero usuario con modelo

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,13 @@
 import { Router }  from 'express'
+import User from './models/User';
 
 const router = Router();
 
 /** Autenticacion y Registro **/
-router.post('/auth/register', (request, response) => {
-    console.log("desde el resgistro")
+
+router.post('/auth/register', async (request, response) => {
+    const user = new User(request.body);
+    await user.save();
 });
 
 export default router;
