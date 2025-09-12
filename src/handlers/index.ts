203845513fb24a97193a478c6b9a19ce0cc17867
richie20502 <richie.lugo.recillas.1990@gmail.com>
2025-09-11 22:36:24 -0600

    validacion de password

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,7 +2,7 @@ import type {Request, Response} from 'express'
 import { validationResult } from 'express-validator';
 import slug from 'slug'
 import User from "../models/User";
-import { hashPassword } from '../utils/auth';
+import { hashPassword, checkPassword } from '../utils/auth';
 
 export const createAccount =  async (request: Request, response: Response) => {
     let errors =  validationResult(request);
@@ -38,13 +38,21 @@ export const login = async (request: Request, response: Response) => {
     if(!errors.isEmpty()){
         return response.status(400).json({errors: errors.array()});
     }
-
     const { email, password } = request.body;
+
     const user = await User.findOne({email});
     if(!user){
         const error = new Error('El usuario no existe !!!');
         return response.status(404).json({error: error.message});
     }
 
-    console.log("desde el login");
+    const isPasswordCorrect = await checkPassword(password, user.password.toString())
+
+    if(!isPasswordCorrect){
+        const error = new Error('Password Incorrecto !!!');
+        return response.status(401).json({error: error.message});
+    }
+
+
+    response.send("Autenticado ..............");
 }
